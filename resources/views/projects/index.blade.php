fb7b939a0063facffa458b15a8f15989a9f2fda3
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2019-01-07 16:51:25 +0000

    Initial setup + tailwindjs

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="flex items-center mb-3">
+		<a href="/projects/create">Create new project</a>
+	</div>
+
+	<ul>
+		@forelse ($projects as $project)
+			<li>
+				<a href="{{ $project->path() }}">{{ $project->title }}</a>
+			</li>
+		@empty
+			<li>No projects yet.</li>
+		@endforelse
+	</ul>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/projects', function () {
-    // validate
-    // persist
-    // redirect
+Route::group(['middleware' => 'auth'], function () {
+	Route::get('/projects', 'ProjectsController@index');
+	Route::get('/projects/create', 'ProjectsController@create');
+	Route::get('/projects/{project}', 'ProjectsController@show');
+	Route::post('/projects', 'ProjectsController@store');
+
+	Route::get('/home', 'HomeController@index')->name('home');
 });
+
+Auth::routes();
+
+
