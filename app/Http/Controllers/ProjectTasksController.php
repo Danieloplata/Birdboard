72495d6409a23561cc7d4a3de3e7f097ca0449e1
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2019-01-17 11:05:32 +0000

    Added tasks and the ability to add them to a project

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Project;
 
 class ProjectTasksController extends Controller
 {
     public function store(Project $project)
     {
+    	request()->validate(['body' => 'required']);
+
     	$project->addTask(request('body'));
 
     	return redirect($project->path());
--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Project;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,13 +14,30 @@ class ProjectTasksTest extends TestCase
     /** @test */
     public function a_project_can_have_tasks()
     {
-       $this->signIn();
+        $this->signIn();
 
-       factory(Project::class)->create(['owner_id' => auth()->id()]);
+        $project = auth()->user()->projects()->create(
+            factory(Project::class)->raw()
+        );
 
-       $this->post($project->path() . '/tasks', ['body' => 'Test task']);
+        $this->post($project->path() . '/tasks', ['body' => 'Test task']);
+        
+        $this->get($project->path())
+            ->assertSee('Test task');
+    }
 
-       $this->get($project->path())
-       		->assertSee('Test task');
+     /** @test */
+    public function a_task_requires_a_body()
+    {
+    	$this->signIn();
+
+    	$project = auth()->user()->projects()->create(
+       		factory(Project::class)->raw()
+       );
+
+        $attributes = factory('App\Task')->raw(['body' => '']);
+
+        $this->post($project->path() . '/tasks', $attributes)
+            ->assertSessionHasErrors('body');
     }
 }
