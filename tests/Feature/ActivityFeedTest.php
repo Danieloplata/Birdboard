6fa7c008759a32d3ab5254f884847c70d7c8f981
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2019-03-04 13:03:12 +0000

    Added activities with tests and project observer

--- tests/Feature/ActivityFeedTest.php
+++ tests/Feature/ActivityFeedTest.php
@@ -11,10 +11,23 @@ class ActivityFeedTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    function creating_a_project_generate_activity()
+    function creating_a_project_generates_activity()
     {
         $project = ProjectFactory::create();
 
         $this->assertCount(1, $project->activity);
+
+        $this->assertEquals('created', $project->activity[0]->description);
+    }
+
+    /** @test */
+    function updating_a_project_generates_activity()
+    {
+        $project = ProjectFactory::create();
+
+        $project->update(['title' => 'changed']);
+
+        $this->assertCount(2, $project->activity);
+        $this->assertEquals('updated', $project->activity->last()->description);
     }
 }
