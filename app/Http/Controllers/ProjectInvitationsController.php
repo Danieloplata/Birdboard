9c9280a3c5fe86be84982d038ec5709f1f215434
Danieloplata <37848327+Danieloplata@users.noreply.github.com>
2019-06-12 18:24:18 +0100

    Moved invitation request to a dedicated request class

--- app/Http/Controllers/ProjectInvitationsController.php
+++ app/Http/Controllers/ProjectInvitationsController.php
@@ -3,22 +3,19 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\ProjectInvitationRequest;
 use App\Project;
 use App\User;
 use Illuminate\Http\Request;
 
 class ProjectInvitationsController extends Controller
 {
-    public function store(Project $project)
+    public function store(Project $project, ProjectInvitationRequest $request)
     {
-        request()->validate([
-            'email' => 'exists:users,email'
-        ], [
-            'email.exists' => 'The user you are inviting must have an account'
-        ]);
-
         $user = User::whereEmail(request('email'))->first();
 
         $project->invite($user);
+
+        return redirect($project->path());
     }
 }
